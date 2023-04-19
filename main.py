2b47e973429307ffee0389f5142023cb4610a6c7
paschoal-artur <paschoal.artur@alu.ufc.br>
2023-04-19 19:31:00 -0300

    code final

---
+++ main.py
@@ -0,0 +1,18 @@
+#Receiving information from user
+num = int(input("Digite um número: \n"))
+
+# Operations with information
+n = 2
+count = 0
+
+# Loop
+while count < num :
+    is_prime = True
+    for i in range(2, n) :
+        if n % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(n)
+        count += 1
+    n += 1
